730436e667cc114b5b0c5d9f3e02d15581f54e2c
Satriobagusimanulloh <79983881+Satriobagusimanulloh@users.noreply.github.com>
2021-04-11 18:35:11 +0700

    Add files via upload

---
+++ pertemuan 6/teori/Bicycle.java
@@ -0,0 +1,26 @@
+package Transportasi;
+
+public class Bicycle {
+    private String Warna = "Hitam";
+    private String Merek;
+
+    public Bicycle() {
+
+    }
+    public Bicycle(String Warna, String Merek) {
+        this.Warna = Warna;
+        this.Merek = Merek;
+    }
+    public void setWarna(String Warna) {
+        this.Warna = Warna;
+    }
+    public String getWarna() {
+        return this.Warna;
+    }
+    public void setMerek(String Merek) {
+        this.Merek = Merek;
+    }
+    public String getMerek() {
+        return this.Merek;
+    }
+}
